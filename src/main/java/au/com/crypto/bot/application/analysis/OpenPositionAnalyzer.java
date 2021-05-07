0942aa71063bc8fafc385adee2b780acdb37e727
Krishna Singampalli <singampk@gmail.com>
2021-05-07 22:40:03 +1000

    removing placing signal when there is an active signal

--- src/main/java/au/com/crypto/bot/application/analysis/OpenPositionAnalyzer.java
+++ src/main/java/au/com/crypto/bot/application/analysis/OpenPositionAnalyzer.java
@@ -72,9 +72,9 @@ public class OpenPositionAnalyzer extends StrategyAnalyzer implements Runnable {
                                 processedEvents.add(marketEvent.getId());
 
                             } else if (!openSignals.isEmpty()) {
-                                trader.raiseSignal(ac, openSignals.get(0).getSignalId(), marketEvent.getPrice().doubleValue(), symbol,
-                                        CONSTANTS._open, sp.getPositionType(), conditionsGroup, sp.getStrategyName(),
-                                        props, marketEvent.getMarket(), marketEvent.getContracts());
+//                                trader.raiseSignal(ac, openSignals.get(0).getSignalId(), marketEvent.getPrice().doubleValue(), symbol,
+//                                        CONSTANTS._open, sp.getPositionType(), conditionsGroup, sp.getStrategyName(),
+//                                        props, marketEvent.getMarket(), marketEvent.getContracts());
 
                                 Log.information("{Application} Found an existing in active signal - {Strategy}" +
                                                 "--- Position Status {PositionStatus}" +
