c85b25135a85021e9d406a0f712eb29c530d47df
Krishna Singampalli <singampk@gmail.com>
2021-06-27 21:00:25 +1000

    formatting

--- src/main/java/au/com/crypto/bot/application/web/pushover/PushoverHelper.java
+++ src/main/java/au/com/crypto/bot/application/web/pushover/PushoverHelper.java
@@ -12,14 +12,13 @@ public class PushoverHelper {
     public static void sendPushOverMessage(String message) {
         try {
             PushoverClient client = new PushoverRestClient();
-            if (!StringUtils.isEmpty(System.getenv("Pushover__AppKey")) && !StringUtils.isEmpty(System.getenv("Pushover__UserKey"))) 
+            if (!StringUtils.isEmpty(System.getenv("Pushover__AppKey")) && !StringUtils.isEmpty(System.getenv("Pushover__UserKey")))
                 //For MVP - just a message, we can customize the message by adding https://support.pushover.net/i44-example-code-and-pushover-libraries#java
                 client.pushMessage(PushoverMessage.builderWithApiToken(System.getenv("Pushover__AppKey"))
                         .setUserId(System.getenv("Pushover__UserKey"))
                         .setMessage(message)
                         .build());
-
-
+            
         }catch (Exception e) {
             Log.error(e, "Error raising alert with pushover");
         }
